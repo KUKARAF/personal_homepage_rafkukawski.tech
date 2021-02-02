57c22ff597e7d42c1035de52c0e3c8a83f9acbdb
root <root@rafalkukawski.tech>
2021-02-02 08:40:51 +0000

    removed more unnecesarry files

--- rafatech/rafatech/secret.py
+++
@@ -1,9 +0,0 @@
-secret_settings_key = 'bl(05dq5#0bm3_rbb2ydskak2!$owcm^$s@c#wyq42j0' 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rafal.kuka94@gmail.com'
-EMAIL_HOST_PASSWORD = 'falhcyfqpudodkox' #past the key or password app here
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'default from email'
-
