5bfc24702b653d0e6dc674b4b5c14708b357739f
Rafał Kukawski <rafal.kuka94@gmail.com>
2024-11-16 20:46:13 +0100

    fix: Correct Django settings module path in wsgi.py

--- rafatech/rafatech/wsgi.py
+++ rafatech/rafatech/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rafatech.rafatech.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rafatech.settings')
 
 application = get_wsgi_application()
