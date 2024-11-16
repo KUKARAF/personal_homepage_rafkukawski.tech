e27044b27336f6bec57e65d5a7de204e3295b4ab
Rafał Kukawski <rafal.kuka94@gmail.com>
2024-11-17 00:26:41 +0100

    small qol improvements

--- rafatech/rafatech/settings.py
+++ rafatech/rafatech/settings.py
@@ -42,7 +42,7 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'rafal.kuka94@gmail.com'
 
 # Static files configuration
-STATIC_ROOT = '/app/staticfiles'
+STATIC_ROOT = '/app/rafatech/static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
